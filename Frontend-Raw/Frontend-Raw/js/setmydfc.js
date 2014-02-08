609b0bb4fc5691ba51637466fe5574e00f376110
GiggleLiu <cacate0129@gmail.com>
2014-02-08 12:58:18 +0800

    Date: 20140208

--- Frontend-Raw/Frontend-Raw/js/setmydfc.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(e) {
-	//browser=checkbrowser();
-	$('#left_col div').click(function(){
-		$('#left_col div').removeClass("selected");
-		$(this).addClass("selected");
-		$('#subframe').attr("src",$(this).attr("id")+'.html');
-	});
-});
--- Frontend-Raw/Frontend-Raw/js/settest.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(e) {
-	move_vis("font_table",[100,100],[200,200],100);
-});
