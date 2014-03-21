aea8408e3121447b71d00fdad757f5ebf391784f
Mingzhi Zeng <zeostudio@gmail.com>
2014-03-21 20:22:30 +0800

    case fix

--- DFC/dfc/settings.py
+++ DFC/dfc/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/giggle/DFC-website/DFC/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -123,6 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'core',
     'activity',
+    'photo',
+    'intelliparser',
     'crispy_forms',
 )
 
