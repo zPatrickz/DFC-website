74dee53589e3643e18b58dc5e335ba3e28789f06
GiggleLiu <cacate0129@gmail.com>
2014-03-21 16:57:11 +0800

    Update Front End.

---
+++ DFC/dfc/local_settings.py
@@ -0,0 +1,22 @@
+# Django settings for dfc project.
+import os
+
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
+    }
+}
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediaroot')
+
+MEDIA_URL = '/media/'
