378d8a9f07e1a2013e1532c0bea30c81c6b13e4e
GiggleLiu <cacate0129@gmail.com>
2014-04-01 17:38:48 +0800

    fix share button & set min-width

--- DFC/static/js/global.js
+++ DFC/static/js/global.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 	});
 	$(".hover-div").hover(
 		function() {
+			share(this.id);
 		   	if($("#"+this.id+'_content').hasClass("hide")){
 		   		$("#"+this.id+'_content').removeClass("hide");
 		   	}
@@ -22,20 +23,20 @@ $(document).ready(function(){
 	   	}
 	);
 	
+
+});
+
+
+function share(content){
 	window._bd_share_config = {
 		common : {
-			bdText : '自定义分享内容',	
+			bdText : content,	
 			bdDesc : '自定义分享摘要',	
 			bdUrl : '自定义分享url地址', 	
 			bdPic : '自定义分享图片'
 		},
 		share : [{
 			"bdSize" : 16
-		}],
-		slide : [{	   
-			bdImg : 0,
-			bdPos : "right",
-			bdTop : 100
 		}],
 		image : [{
 			viewType : 'list',
@@ -46,4 +47,4 @@ $(document).ready(function(){
 		}]
 	}
 	with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];
-});
+}
