c2171c8ec4e4c2824d6308f7fb323e73d525df78
chuang xie <chuangxie0111@gmail.com>
2014-04-03 00:23:28 +0800

    login logout

--- DFC/accounts/views.py
+++ DFC/accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from forms import OrganizationCreationForm, UserCreationForm
+from forms import OrganizationCreationForm, UserCreationForm, SignInForm
 
 
 def register(request):
@@ -28,17 +29,30 @@ def signup_organization(request):
             'form': form,
     }, context_instance=RequestContext(request))
 
+def login(request):
+    form = SignInForm()
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            email, password, remember_me = (form.cleaned_data['email'], 
+                form.cleaned_data['password'], 
+                form.cleaned_data['remember_me'])
+            user = authenticate(email=email, password=password)
+            auth_login(request, user)
+            if remember_me:
+                request.session.set_expiry(24*60*60)
+            else:
+                request.session.set_expiry(0)
+            return HttpResponseRedirect(reverse('index'))
+    return render_to_response('accounts/login.html', {
+        'form': form, 
+    }, context_instance=RequestContext(request))
+
+def logout(request):
+    if request.user.is_authenticated():
+        auth_logout(request)
+        return HttpResponseRedirect(reverse('index'))
+
 @login_required
 def user_profile(request):
     pass
-    #if request.method == 'POST':
-    #    form = UserProfileForm(request.POST, instance=request.user.profile)
-    #    if form.is_valid():
-    #        form.save()
-    #        return HttpResponseRedirect('accounts/profile.html')
-    #else:
-    #    form = UserProfileForm(instance=request.user.profile)
-    #return render_to_response('accounts/profile.html', {
-    #    'form': form,
-    #    'user': request.user,
-    #    }, context_instance=RequestContext(request))
